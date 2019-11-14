54256e5af269ea81864926db7e6dfdbfaf552a8f
Eirik S. Morland <eirik@morland.no>
2019-11-14 15:30:45 +0100

    More better

--- clean-repo-check.sh
+++ clean-repo-check.sh
@@ -1,3 +1,15 @@
 set -e
-../../vendor/bin/drush csex -y
-[[ -z $(git status -s) ]] || exit 1
+
+wdir="$PWD"; [ "$PWD" = "/" ] && wdir=""
+case "$0" in
+  /*) scriptdir="${0}";;
+  *) scriptdir="$wdir/${0#./}";;
+esac
+scriptdir="${scriptdir%/*}"
+
+$scriptdir/../../../vendor/bin/drush cex -y
+if [[ $(git status -s | wc -c) -ne 0 ]]; then
+    echo "Working tree is not clean:"
+    git status
+    exit 1
+fi
