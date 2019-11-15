32335527e9af226c1dd2322fe66de52624b53dd3
Eirik S. Morland <eirik@morland.no>
2019-11-15 12:14:52 +0100

    Remove drush dep. Compatible with sh

--- clean-repo-check.sh
+++ clean-repo-check.sh
@@ -1,14 +1,6 @@
 set -e
 
-wdir="$PWD"; [ "$PWD" = "/" ] && wdir=""
-case "$0" in
-  /*) scriptdir="${0}";;
-  *) scriptdir="$wdir/${0#./}";;
-esac
-scriptdir="${scriptdir%/*}"
-
-$scriptdir/../../../vendor/bin/drush cex -y
-if [[ $(git status -s | wc -c) -ne 0 ]]; then
+if [ $(git status -s | wc -c) -ne 0 ]; then
     echo "Working tree is not clean:"
     git status
     exit 1
