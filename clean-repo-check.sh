8a264d170206a250f28b5e8dbbab7246f72d939f
Eirik Stanghelle Morland <eirik@morland.no>
2023-06-08 09:32:54 +0200

    More verbose error output

--- clean-repo-check.sh
+++ clean-repo-check.sh
@@ -1,7 +1,9 @@
 set -e
 
 if [ $(git status -s | wc -c) -ne 0 ]; then
-    echo "Working tree is not clean:"
     git status
+    echo "!ERROR-------------------!ERROR"
+    echo "The working tree is not clean. There are either changed or uncommited files. Please check the output of git status above for more information about this failure"
+    echo "!ERROR-------------------!ERROR"
     exit 1
 fi
